9c6e07fb3507f770a0c78aab351f50e4c09ea88a
admin <admin>
2023-07-17 07:03:39 +0000

    files: [None, None, None] added to repository

---
+++ dags/spark/test7_spark_job.py
@@ -0,0 +1,54 @@
+import os
+from pyspark.sql import SparkSession
+from datetime import datetime
+
+def init_feature_store_path():
+    from feast import FeatureStore
+    with open("feature_store.yaml", "w") as yaml_file:
+        yaml_file.write(os.getenv("FEATURE_STORE_YAML"))
+    return FeatureStore(repo_path=".")
+
+def prepare_dataset(store, batch,**kwargs):
+
+    feature_service = store.get_feature_service(feature_service_1)
+    enriched_job = store.get_historical_features(
+        entity_df=batch,
+        features=feature_service,
+    )
+    return enriched_job.to_spark_df()
+
+if __name__ == "__main__":
+    store = init_feature_store_path()
+    conf = SparkConf().set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")\
+    .set("spark.sql.catalogImplementatio "hive")\
+    .set("spark.hadoop.hive.metastore.uris", os.environ.get("METASTORE_URI"))\
+    .set("spark.sql.warehouse.dir", os.environ.get("METASTORE_WAREHOUSE_DIR"))\
+    .set("spark.hadoop.fs.s3a.access.key", os.environ.get("METASTORE_AWS_ACCESS_KEY_ID"))\
+    .set("spark.hadoop.fs.s3a.secret.key", os.environ.get("METASTORE_AWS_SECRET_ACCESS_KEY"))\
+    .set("spark.hadoop.fs.s3a.endpoint", os.environ.get("FEAST_S3_ENDPOINT_URL"))\
+    .set("spark.sql.debug.maxToStringFields", "100")\
+    .set("spark.hadoop.fs.s3a.path.style.access", "true")\
+    .set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")\
+    .set("spark.hadoop.fs.s3a.connection.ssl.enabled", "false")\
+    .set("spark.driver.extraJavaOptions", "-Divy.cache.dir=/tmp -Divy.home=/tmp")\
+    .set("spark.executor.extraJavaOptions", "-Divy.cache.dir=/tmp -Divy.home=/tmp")\
+
+    sc = SparkContext.getOrCreate(conf=conf)
+    spark = SparkSession(sc).builder.appName("test7_predict").enableHiveSupport().getOrCreate()
+    df = f"select * from cdm.nvg_data_gr_agr_all"
+
+    enriched_df = prepare_dataset(store, df)
+    import mlflow
+
+    # Load model as a Spark UDF.
+    loaded_model = mlflow.pyfunc.spark_udf(spark, model_uri="models:/breast_cancer/2")
+    input_df = enriched_df[enriched_df.columns.drop("event_timestamp")]
+    # Predict on a Spark DataFrame.
+    columns = list(input_df.columns)
+    df = loaded_model(*columns)
+    df = df.withColumn('date_part', datetime.now().strftime('%Y-%m-%d'))
+    if spark._jsparkSession.catalog().tableExists('cdm', 'output_storage'):
+        df.write.partitionBy("date_part").format("parquet").mode("append").insertInto(f"cdm.output_storage")
+    else:
+        df.write.partitionBy("date_part").format("parquet").mode("append").saveAsTable(f"cdm.output_storage")
+    spark.stop()
---
+++ dags/test7.py
@@ -0,0 +1,34 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
+default_args = {
+                'owner': 'admin',
+                'retry_delay': timedelta(minutes=5),
+                'max_active_runs': 1
+               }
+
+dag = DAG(
+    'test7',
+    default_args=default_args,
+    description='submit spark-app as sparkApplication on kubernetes',
+    schedule_interval='0 7 * * 1',
+    start_date=datetime(2023,7,21,8,30),
+    catchup=False,
+)
+
+t1 = SparkKubernetesOperator(
+    task_id='spark_submit',
+    namespace="dognauts-mtp",
+    application_file="spark/test7_spark_manifest.yaml",
+    do_xcom_push=True,
+    dag=dag,
+)
+
+t2 = SparkKubernetesSensor(
+    task_id='spark_monitor',
+    namespace="dognauts-mtp",
+    application_name="{ task_instance.xcom_pull(task_ids='spark_submit')['metadata']['name'] }",
+    dag=dag,
+)
+t1 >> t2
